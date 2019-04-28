7d0370e6e5fc78e8c0f8ad0fe88a23e4acb43665
dawitkwm <dawitkahsay@gmail.com>
2019-04-28 15:47:27 -0400

    passing 'should render Tile component' spec

---
+++ src/components/tile/tile.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Tile extends Component {
+  state = {};
+  render() {
+    return <input className="tile" type="button" value="X" />
+  }
+}
+
+export default Tile;
--- src/components/tile/tile.spec.jsx
+++ src/components/tile/tile.spec.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Tile from './tile';
-import { idText } from 'typescript';
 
 describe('Tile', () => {
     it('should render Tile component', () => {
