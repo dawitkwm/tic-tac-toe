8e39808167412a42aad7bb29a11cb46c8ebe4f87
dawitkwm <dawitkahsay@gmail.com>
2019-04-28 22:39:53 -0400

    failing board 'should have nine tiles' spec

--- src/components/board/board.spec.jsx
+++ src/components/board/board.spec.jsx
@@ -8,4 +8,9 @@ describe("Board component", () => {
     const boardComponent = shallow(<Board />);
     expect(boardComponent).toMatchSnapshot();
   });
+
+  it("should have nine tiles", () => {
+    const boardComponent = shallow(<Board />);
+    expect(boardComponent.state("tiles").length).toEqual(9);
+  });
 });
