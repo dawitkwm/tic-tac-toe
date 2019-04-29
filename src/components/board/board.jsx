f08de13e067abc24c4a4595da844dfd14fcf89b9
dawitkwm <dawitkahsay@gmail.com>
2019-04-28 22:41:25 -0400

    passing board 'should have nine tiles' spec

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 import Tile from "../tile/tile";
 
 class Board extends Component {
-  state = {};
+  state = {
+    tiles: Array(9).fill(null)
+  };
   render() {
     return <Tile />;
   }
