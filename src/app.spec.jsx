41ae291c1397488211ee1953cbd6fa025d8f3434
dawitkwm <dawitkahsay@gmail.com>
2019-04-30 07:10:02 -0400

    failing 'should render game App component' spec

---
+++ src/app.spec.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import App from "./app";
+
+describe("App component", () => {
+  it("should render game App component", () => {
+    const appWrapper = shallow(<App />);
+    expect(appWrapper).toMatchSanpshot();
+  });
+});
