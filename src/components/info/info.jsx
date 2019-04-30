a1fb76e3e76b3bed8c48174db9bc0a91b7a09e88
dawitkwm <dawitkahsay@gmail.com>
2019-04-30 05:38:48 -0400

    passing 'should render Info component' spec

---
+++ src/components/info/info.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import "./info.css";
+
+class Info extends Component {
+  render() {
+    const statusInfo =
+      "game status indicator: whose turn is it or who won the game";
+    return <header>statusInfo</header>;
+  }
+}
+
+export default Info;
